7efbed463544c9ef4c0a5c4e80027abfe257c11c
Pablo-henrique23 <pablohenriquesilva26@gmail.com>
2024-06-24 00:06:00 -0300

    swww animation set

--- .config/scripts/rofi-wallpaperpicker.sh
+++ .config/scripts/rofi-wallpaperpicker.sh
@@ -56,7 +56,7 @@ wall_selection=$(find "${wall_dir}" -type f \( -iname "*.jpg" -o -iname "*.jpeg"
 
 # Set the wallpaper
 [[ -n "$wall_selection" ]] || exit 1
-swww img "${wall_dir}/${wall_selection}" --transition-step 20 --transition-fps 20
+swww img "${wall_dir}/${wall_selection}" --transition-step 30 --transition-fps 60 -t random
 
 # Troca o tema de tudo
 wal -i "${wall_dir}/${wall_selection}"
