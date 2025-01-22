fb0e0e1c9fd9d7b511d0719e7368efec840ec682
Pablo-henrique23 <pablohenriquesilva26@gmail.com>
2025-01-22 11:51:54 -0300

    add stow to installation

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-sudo pacman -Syu hyprland waybar swww dunst hyprlock rofi hypridle hyprpicker xdg-desktop-portal-hyprland pipewire wireplumber hyprcursor cliphist grim slurp gwenview python-pywal sddm network-manager-applet fastfetch zsh ghostty
+sudo pacman -Syu stow hyprland waybar swww dunst hyprlock rofi hypridle hyprpicker xdg-desktop-portal-hyprland pipewire wireplumber hyprcursor cliphist grim slurp gwenview python-pywal sddm network-manager-applet fastfetch zsh ghostty
 
 yay -S hyprshade pokemon-colorscripts-git
