1a3169e5b4b0191ea300cb873726874b76072634
Pablo-henrique23 <pablohenriquesilva26@gmail.com>
2025-01-22 12:00:48 -0300

    esvazia pasta root

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-sudo pacman -Syu stow hyprland waybar swww dunst hyprlock rofi hypridle hyprpicker xdg-desktop-portal-hyprland pipewire wireplumber hyprcursor cliphist grim slurp gwenview python-pywal sddm network-manager-applet fastfetch zsh ghostty
+sudo pacman -Syu stow hyprland waybar swww dunst hyprlock rofi hypridle hyprpicker xdg-desktop-portal-hyprland pipewire wireplumber hyprcursor cliphist grim slurp gwenview python-pywal sddm network-manager-applet fastfetch zsh ghostty bc
 
 yay -S hyprshade pokemon-colorscripts-git
