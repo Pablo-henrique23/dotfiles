511505aacc20e5204466894a2902a515054cd2aa
Pablo-henrique23 <pablohenriquesilva26@gmail.com>
2025-01-30 00:01:04 -0300

    att install script

--- .install.sh
+++ .install.sh
@@ -2,6 +2,6 @@
 
 sudo pacman -Syu stow hyprland waybar swww dunst hyprlock rofi hypridle hyprpicker xdg-desktop-portal-hyprland pipewire wireplumber hyprcursor cliphist grim slurp gwenview python-pywal sddm network-manager-applet fastfetch zsh ghostty bc
 
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && yay -Y --gendb && yay -Syu --devel
 
 yay -S hyprshade pokemon-colorscripts-git
