1560959da287498486fca6af2d4afdf0bda6163e
Pablo Henrique <pablohenriquesilva26@gmail.com>
2025-03-23 22:43:43 -0300

    waybar changer

--- .config/scripts/reloadWaybar.sh
+++ .config/scripts/reloadWaybar.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# Definição dos valores padrão
+DEFAULT_CONFIG="$HOME/dotfiles/.config/waybar/configs/config2.jsonc"
+DEFAULT_STYLE="$HOME/dotfiles/.config/waybar/configs/colored.css"
+
+# Obtém os argumentos da linha de comando ou usa os padrões
+CONFIG_PATH=${1:-$DEFAULT_CONFIG}
+STYLE_PATH=${2:-$DEFAULT_STYLE}
+
+# Mata a Waybar e inicia com os arquivos especificados
 killall waybar
-waybar -c ~/dotfiles/.config/waybar/configs/config2.jsonc -s ~/dotfiles/.config/waybar/configs/colored.css
+waybar -c "$CONFIG_PATH" -s "$STYLE_PATH" &
---
+++ .config/scripts/waybarChanger.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CONFIG_DIR="$HOME/dotfiles/.config/waybar/configs"
+STYLE_DIR="$CONFIG_DIR"
+
+OPTIONS=( $(ls "$CONFIG_DIR" | grep '\.jsonc$' | sed 's/\.jsonc$//') )
+STYLES=( $(ls "$STYLE_DIR" | grep '\.css$' | sed 's/\.css$//') )
+
+SELECTION=$(printf "%s\n" "${OPTIONS[@]}" | rofi -dmenu -p "Escolha um perfil")
+
+STYLE_SELECTION=$(printf "%s\n" "${STYLES[@]}" | rofi -dmenu -p "Escolha um estilo")
+
+if [[ -n "$SELECTION" && -f "$CONFIG_DIR/$SELECTION.jsonc" && -n "$STYLE_SELECTION" && -f "$STYLE_DIR/$STYLE_SELECTION.css" ]]; then
+    echo "$CONFIG_DIR/$SELECTION.jsonc" > $HOME/dotfiles/.config/waybar/current_config.jsonc
+    echo "$CONFIG_DIR/$STYLE_SELECTION.css" > $HOME/dotfiles/.config/waybar/current_style.css
+    killall waybar
+    ~/dotfiles/.config/scripts/reloadWaybar.sh "$CONFIG_DIR/$SELECTION.jsonc" "$STYLE_DIR/$STYLE_SELECTION.css"
+
+else
+    notify-send -t 2000 "Waybar" "Nenhuma configuração ou estilo válido selecionado."
+fi
+
