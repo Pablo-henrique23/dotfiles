06b13586cbe197415c28b845755a25d4997c8ddb
Pablo Henrique <pablohenriquesilva26@gmail.com>
2025-10-27 19:37:35 -0300

    .

--- .install.sh
+++ .install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-sudo pacman -Syu git stow hyprland waybar swww dunst hyprlock rofi hypridle hyprpicker xdg-desktop-portal-hyprland pipewire wireplumber hyprcursor cliphist grim slurp gwenview python-pywal sddm network-manager-applet fastfetch zsh ghostty bc ninja meson cmake make alacritty yazi pcmanfm
+sudo pacman -Syu git stow hyprland waybar swww dunst hyprlock rofi hypridle hyprpicker xdg-desktop-portal-hyprland pipewire wireplumber hyprcursor cliphist grim slurp gwenview python-pywal sddm network-manager-applet fastfetch zsh ghostty bc ninja meson cmake make alacritty yazi pcmanfm btop
 
 if ! command -v yay &> /dev/null; then
 	sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && yay -Y --gendb && yay -Syu --devel
