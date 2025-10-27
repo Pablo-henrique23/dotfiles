4ea95abe09b30e54d65e4c4c430515df91f9219e
Pablo Henrique <pablohenriquesilva26@gmail.com>
2025-10-27 19:19:12 -0300

    -m

--- .config/scripts/waybarChanger.sh
+++ .config/scripts/waybarChanger.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CONFIG_DIR="$HOME/dotfiles/.config/waybar/configs"
-STYLE_DIR="$CONFIG_DIR"
+CONFIG_DIR="$HOME/dotfiles/.config/waybar/configs/configs"
+STYLE_DIR="$HOME/dotfiles/.config/waybar/configs/styles"
 
 OPTIONS=( $(ls "$CONFIG_DIR" | grep '\.jsonc$' | sed 's/\.jsonc$//') )
 STYLES=( $(ls "$STYLE_DIR" | grep '\.css$' | sed 's/\.css$//') )
@@ -13,7 +13,7 @@ STYLE_SELECTION=$(printf "%s\n" "${STYLES[@]}" | rofi -dmenu -p "Escolha um esti
 if [[ -n "$SELECTION" && -f "$CONFIG_DIR/$SELECTION.jsonc" && -n "$STYLE_SELECTION" && -f "$STYLE_DIR/$STYLE_SELECTION.css" ]]; then
     
     echo "$CONFIG_DIR/$SELECTION.jsonc" > $HOME/dotfiles/.config/waybar/current_config.jsonc
-    echo "$CONFIG_DIR/$STYLE_SELECTION.css" > $HOME/dotfiles/.config/waybar/current_style.css
+    echo "$STYLE_DIR/$STYLE_SELECTION.css" > $HOME/dotfiles/.config/waybar/current_style.css
 
     killall waybar
     ~/dotfiles/.config/scripts/reloadWaybar.sh
