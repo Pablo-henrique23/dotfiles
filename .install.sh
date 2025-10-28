4db7cefc72ed836d1890ab2a00d7788b6612c57f
Pablo-henrique23 <pablohenriquesilva26@gmail.com>
2025-10-27 22:08:37 -0300

    inetutils

--- .install.sh
+++ .install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 
-sudo pacman -Syu git stow hyprland waybar swww dunst hyprlock rofi hypridle hyprpicker xdg-desktop-portal-hyprland pipewire wireplumber hyprcursor cliphist grim slurp gwenview python-pywal sddm network-manager-applet fastfetch zsh ghostty bc ninja meson cmake make alacritty yazi pcmanfm btop
+sudo pacman -Syu git stow hyprland waybar swww dunst hyprlock rofi hypridle hyprpicker xdg-desktop-portal-hyprland pipewire wireplumber hyprcursor cliphist grim slurp gwenview python-pywal sddm network-manager-applet fastfetch zsh ghostty bc ninja meson cmake make alacritty yazi pcmanfm btop inetutils
 
 if ! command -v yay &> /dev/null; then
-	sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && yay -Y --gendb && yay -Syu --devel
+	sudo pacman -S --needed git base-devel && cd .. && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && yay -Y --gendb && yay -Syu --devel
+fi
 
 yay -S hyprshade pokemon-colorscripts-git
 
-stow .
+cd dotfiles && stow .
