d34bc53a316d2beddd524f4ef584c16939fff798
Pablo-henrique23 <pablohenriquesilva26@gmail.com>
2025-10-27 23:37:15 -0300

    .

--- .install.sh
+++ .install.sh
@@ -3,7 +3,7 @@
 sudo pacman -Syu git stow hyprland waybar swww dunst hyprlock rofi hypridle hyprpicker xdg-desktop-portal-hyprland pipewire wireplumber hyprcursor cliphist grim slurp gwenview python-pywal sddm network-manager-applet fastfetch zsh ghostty bc ninja meson cmake make alacritty yazi pcmanfm btop inetutils
 
 # coringas
-sudo pacman -S ufw gufw ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer
+sudo pacman -S ufw gufw ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer fwupd zip vlc 
 
 if ! command -v yay &> /dev/null; then
 	sudo pacman -S --needed git base-devel && cd .. && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && yay -Y --gendb && yay -Syu --devel
@@ -12,3 +12,5 @@ fi
 yay -S hyprshade pokemon-colorscripts-git
 
 cd dotfiles && stow .
+
+# se estiver no KDE, sudo pacman -S flatpak packagekit-qt6
