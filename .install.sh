c57e586548c00287096b323f03e0c140dee9cee8
Pablo-henrique23 <pablohenriquesilva26@gmail.com>
2025-10-27 22:09:47 -0300

    install

--- .install.sh
+++ .install.sh
@@ -2,6 +2,9 @@
 
 sudo pacman -Syu git stow hyprland waybar swww dunst hyprlock rofi hypridle hyprpicker xdg-desktop-portal-hyprland pipewire wireplumber hyprcursor cliphist grim slurp gwenview python-pywal sddm network-manager-applet fastfetch zsh ghostty bc ninja meson cmake make alacritty yazi pcmanfm btop inetutils
 
+# coringas
+sudo pacman -S ufw gufw ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer
+
 if ! command -v yay &> /dev/null; then
 	sudo pacman -S --needed git base-devel && cd .. && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && yay -Y --gendb && yay -Syu --devel
 fi
